d181d7affcba25cbcc5905834be19e01a4bcede9
CHANDRA SHUDHANSHU <88597159+chandrashu@users.noreply.github.com>
2025-12-14 17:01:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const texts = ["Web Developer", "Frontend Developer", "UI Designer"];
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+  if (count === texts.length) {
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector(".typing").textContent = letter;
+
+  if (letter.length === currentText.length) {
+    setTimeout(() => {
+      index = 0;
+      count++;
+    }, 1500);
+  }
+  setTimeout(type, 150);
+})();
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("active");
+});
+
+/* Close menu on link click (mobile) */
+document.querySelectorAll(".nav-links a").forEach(link => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navLinks.classList.remove("active");
+  });
+});
+
